69aa74e22cecf853386f895fbc15f939b871e545
Hemanth-Moka <2300030439@kluniversity.in>
2025-07-30 10:43:43 +0530

    Upadted App .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ function App() {
     <div className="container">
       <h1>Welcome to My React Project - S105</h1>
       <p>CI/CD and Cloud DevOps</p>
+      <h2>This is second Versioin</h2>
       <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
-      <p>Done with Git Pages & Git Actions</p>
+      {/* <p>Done with Git Pages & Git Actions</p> */}
     </div>
   );
 }
