e2678dc684ef16e63d288f4e0b9b24cf1a2abc6d
Hemanth-Moka <2300030439@kluniversity.in>
2025-07-31 11:53:31 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <p>CI/CD and Cloud DevOps</p>
       <h2>This is second Versioin</h2>
       <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
-      {/* <p>Done with Git Pages & Git Actions</p> */}
+      <h1>Done with Git Pages & Git Actions</h1>
     </div>
   );
 }
