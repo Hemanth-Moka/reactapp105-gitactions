e7dee39bfd780dd0c276aec8a16e56c740b222f8
Hemanth-Moka <2300030439@kluniversity.in>
2025-07-31 12:36:34 +0530

    Updated codes

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ function App() {
     <div className="container">
       <h1>Welcome to My React Project - S105</h1>
       <p>CI/CD and Cloud DevOps</p>
-      <h2>This is second Versioin</h2>
+      <h2>This is second Version</h2>
       <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
       <h1>Done with Git Pages & Git Actions</h1>
+      <p>This Is Second Version LAB3</p>
     </div>
   );
 }
